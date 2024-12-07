6f5dd8ba4bf150d47ba5e99a91da1470128875b4
Joshua Graber <joshua.d.graber@gmail.com>
2024-12-07 11:49:32 -0500

    fix: miscellaneous lint and test fixes

---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config.InitialOptions} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  // coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '**/*index.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.d.ts',
+    '!**/{index,config}.ts',
+  ],
+  coverageReporters: ['text', 'lcov', 'json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+module.exports = config;
