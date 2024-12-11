64a9857860c473487e948a9a88b64874e3f1817e
Joshua Graber <joshua.d.graber@gmail.com>
2024-12-11 09:09:20 -0500

    chore: attempt new thing and add logs

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
   DONOR_BOX_AUTH_ALIAS: process.env.DONOR_BOX_AUTH_ALIAS,
   DONOR_BOX_AUTH_SECRET: process.env.DONOR_BOX_AUTH_SECRET,
-  ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+  ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS ?? '*',
   NODE_ENV: process.env.NODE_ENV || 'production',
   PORT: process.env.PORT,
   HOST: process.env.HOST,
--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import corsProxy from 'cors-anywhere';
 // Listen on a specific host via the HOST environment variable
 const host = config.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-const port = config.PORT || 8080;
+const port = config.PORT || 3000;
 
 const server = corsProxy.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
+  httpProxyOptions: {
+    secure: false,
+  },
+  originWhitelist: config.ALLOWED_ORIGINS === '*' 
+    ? [] // Empty array means allow all origins
+    : config.ALLOWED_ORIGINS.split(','), // Or split string of allowed origins
+  // requireHeader: ['x-requested-with'],
   removeHeaders: ['cookie', 'cookie2'],
+});
+
+server.on('error', function(err) {
+  console.error('Proxy error:', err);
 });
 
 server.listen(port, host, function () {
